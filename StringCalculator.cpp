a05d26429aed4a49d68839b466bb525a25d15350
vaishukla <168533722+vaishukla@users.noreply.github.com>
2024-06-27 15:54:05 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,6 +1,6 @@
 #include "StringCalculator.h"
 
-int StringCalculator::add(string input){
+/* int StringCalculator::add(string input){
 int sum = 0;
     for (char c : input) {
             sum += convertToDigit(c);
@@ -22,4 +22,46 @@ bool StringCalculator::inRange(char c){
     }
     return false;
 }
+*/
 
+ int StringCalculator::add(string input) {
+        if (input.empty()) {
+            return 0;
+        }
+
+        int sum = 0;
+        std::string delimiter = ",";
+        std::string numbers = input;
+
+        if (input.substr(0, 2) == "//") {
+            delimiter = input.substr(2, 1);
+            numbers = input.substr(4);
+        }
+
+        size_t pos = 0;
+        while ((pos = numbers.find(delimiter)) != std::string::npos) {
+            std::string number = numbers.substr(0, pos);
+            int num = std::stoi(number);
+
+            if (num < 0) {
+                throw std::runtime_error("Negative numbers not allowed");
+            }
+
+            if (num <= 1000) {
+                sum += num;
+            }
+
+            numbers.erase(0, pos + delimiter.length());
+        }
+
+        int lastNumber = std::stoi(numbers);
+        if (lastNumber < 0) {
+            throw std::runtime_error("Negative numbers not allowed");
+        }
+
+        if (lastNumber <= 1000) {
+            sum += lastNumber;
+        }
+
+        return sum;
+    }
