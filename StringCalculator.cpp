d103080d9b9cc579604cc3a8b91786d23af523d5
vaishukla <168533722+vaishukla@users.noreply.github.com>
2024-06-27 16:25:19 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -10,11 +10,18 @@ int sum = 0;
 }
 
 int StringCalculator::convertToDigit(char c) {
-    if (std::isdigit(c)) {
+    if (std::isdigit(c) && c>0) {
         return c - '0';
     }
     return 0;
 }
+
+// int StringCalculator::checkIfNegativeNumber(char c){
+//  if(c<0){
+//   return 0;
+//  }
+//  return c;
+// }
 
 // bool StringCalculator::inRange(char c){
 //     if(c>0 && c<1000){
