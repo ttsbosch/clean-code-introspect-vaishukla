945e332488bd1d1d3ddd46dc7fe9d6e2fe6eff25
vaishukla <168533722+vaishukla@users.noreply.github.com>
2024-06-27 15:12:28 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,18 +1,12 @@
 #include "StringCalculator.h"
 
 int StringCalculator::add(string input){
-  if(input == "" || input == "0")
-  {
-    return 0;
-  }
-  int sum = 0;
-  for (char c : input) 
-  {
-        if (std::isdigit(c)) 
-        {
+int sum = 0;
+    for (char c : input) {
+        if (std::isdigit(c)) {
             sum += c - '0';
         }
     }
-  return sum;
+    return sum;
 }
 
