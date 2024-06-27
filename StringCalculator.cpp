c9bd13b0cf0de6b718c205e768d103c5c9745bb8
vaishukla <168533722+vaishukla@users.noreply.github.com>
2024-06-27 16:13:57 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,6 +1,6 @@
 #include "StringCalculator.h"
 
-/* int StringCalculator::add(string input){
+ int StringCalculator::add(string input){
 int sum = 0;
     for (char c : input) {
             sum += convertToDigit(c);
@@ -10,58 +10,16 @@ int sum = 0;
 }
 
 int StringCalculator::convertToDigit(char c) {
-    if (std::isdigit(c) && inRange(c)) {
+    if (std::isdigit(c)) {
         return c - '0';
     }
     return 0;
 }
 
-bool StringCalculator::inRange(char c){
-    if(c>0 && c<1000){
-        return true;
-    }
-    return false;
-}
-*/
+// bool StringCalculator::inRange(char c){
+//     if(c>0 && c<1000){
+//         return true;
+//     }
+//     return false;
+// }
 
- int StringCalculator::add(string input) {
-        if (input.empty()) {
-            return 0;
-        }
-
-        int sum = 0;
-        std::string delimiter = ",";
-        std::string numbers = input;
-
-        if (input.substr(0, 2) == "//") {
-            delimiter = input.substr(2, 1);
-            numbers = input.substr(4);
-        }
-
-        size_t pos = 0;
-        while ((pos = numbers.find(delimiter)) != std::string::npos) {
-            std::string number = numbers.substr(0, pos);
-            int num = std::stoi(number);
-
-            if (num < 0) {
-                throw std::runtime_error("Negative numbers not allowed");
-            }
-
-            if (num <= 1000) {
-                sum += num;
-            }
-
-            numbers.erase(0, pos + delimiter.length());
-        }
-
-        int lastNumber = std::stoi(numbers);
-        if (lastNumber < 0) {
-            throw std::runtime_error("Negative numbers not allowed");
-        }
-
-        if (lastNumber <= 1000) {
-            sum += lastNumber;
-        }
-
-        return sum;
-    }
