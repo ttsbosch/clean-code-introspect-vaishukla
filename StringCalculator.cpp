0b4ce51087cc2f5950febd2f0cbc9d0e990faad8
vaishukla <168533722+vaishukla@users.noreply.github.com>
2024-06-27 15:27:51 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -4,9 +4,16 @@ int StringCalculator::add(string input){
 int sum = 0;
     for (char c : input) {
         if (std::isdigit(c) && c>0 && c<1000) {
-            sum += c - '0';
+            sum += convertToDigit(c);
         }
-    }
+    }   
     return sum;
+}
+
+int StringCalculator::convertToDigit(char c) {
+    if (std::isdigit(c) && c > 0 && c < 1000) {
+        return c - '0';
+    }
+    return 0;
 }
 
